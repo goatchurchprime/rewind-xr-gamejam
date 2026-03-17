46c32acf98c3c4ec4c17360033148901e1ac4eeb
Julian Todd <julian@goatchurch.org.uk>
2026-03-17 23:14:49 +0000

    greybox some furniture

--- main.gd
+++ main.gd
@@ -56,4 +56,6 @@ func _on_xr_controller_3d_left_button_released(name):
 		tweensnakeout.kill()
 		tweensnakeout = null
 		tweensnakerewind = get_tree().create_tween()
-		tweensnakerewind.tween_method(func (x): $GSnake.animmaterial.set_shader_parameter("texvtime", x), 0.0, 1.0, 2.0)
+		tweensnakerewind.tween_method(func (x): $GSnake.animmaterial.set_shader_parameter("texvtime", x), 0.0, 1.0, 0.4)
+		var u0 = $GSnake.animmaterial.get_shader_parameter("texutime")
+		tweensnakerewind.tween_method(func (x): $GSnake.animmaterial.set_shader_parameter("texutime", x), u0, 1.0, 0.4)
