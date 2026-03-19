6305e27f48edc86405e0a9d94d688c2b991f5bd4
yankscally <yankscally@gmail.com>
2026-03-19 21:39:54 +0000

    muting music

--- audio/code/music.gd
+++ audio/code/music.gd
@@ -6,12 +6,19 @@ var song_2 = preload("res://audio/music/song_2/song_2.tres")
 var song_boss = preload("res://audio/music/song_boss/song_boss.tres")
 
 ##
-var songs = [song_1, song_2, song_boss]
+var songs : Array[AudioStreamSynchronized] = [song_1, song_2, song_boss]
 var current_song = 0
 
 func _ready():
 	finished.connect(on_song_finished)
 	stream = songs[current_song]
+	var now_playing = songs[current_song] as AudioStreamSynchronized
+	# set tracks to muted..
+	now_playing.set("stream_0/volume", -60.0) # drums
+	# keep the bass
+	#now_playing.set("stream_1/volume", -60.0) # bass
+	now_playing.set("stream_2/volume", -60.0) # synth
+	now_playing.set("stream_3/volume", -60.0) # keys
 	play()
 
 func on_song_finished():
