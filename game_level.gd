1820c857bcd541dc84cd3e9ce2be9f877eef5c34
Julian Todd <julian@goatchurch.org.uk>
2026-03-21 17:33:26 +0000

    level transition

--- game_level.gd
+++ game_level.gd
@@ -9,12 +9,25 @@ var scenes = {  "llazlo":"res://llazloscene/level_llazz.tscn",
 var exrdirs = { "llazlo":"res://level_editor/snakeexrs",
 				"lobby":"res://lobbyscene/snakeexrs" }
 
+func setusercontrolpanel(lusercontrolpanel):
+	usercontrolpanel = lusercontrolpanel
+	scenelist = usercontrolpanel.get_node("VBox/HBox2/SceneChoice")
+	scenelist.item_selected.connect(sceneselected)
+
+func Dset_fade(p_value : float):
+	XRToolsFade.set_fade("spawnpoint", Color(0.1, 0.1, 0.1, p_value))
+
 func sceneselected(i):
 	var scenename = scenelist.get_item_text(i)
 	print("Scene selected ", scenename)
 	var sceneres = scenes[scenename]
-	if get_child_count() == 1:
-		var x = get_child(0)
+
+	var fadetween = get_tree().create_tween()
+	fadetween.tween_method(Dset_fade, 0.0, 1.0, 0.34)
+	await fadetween.finished
+
+	if get_child_count() >= 1:
+		var x = get_child(-1)
 		remove_child(x)
 		x.queue_free()
 	add_child(load(sceneres).instantiate())
@@ -22,7 +35,9 @@ func sceneselected(i):
 	snakemonsters.edir = exrdirs[scenename]
 	snakemonsters.loadsnakeexrs()
 
-func setusercontrolpanel(lusercontrolpanel):
-	usercontrolpanel = lusercontrolpanel
-	scenelist = usercontrolpanel.get_node("VBox/HBox2/SceneChoice")
-	scenelist.item_selected.connect(sceneselected)
+	get_node("../XROrigin3D").transform = Transform3D()
+	get_node("../XROrigin3D/PlayerBody").velocity = Vector3(0,0,0)
+	fadetween = get_tree().create_tween()
+	fadetween.tween_method(Dset_fade, 1.0, 0.0, 0.34)
+	await fadetween.finished
+	fadetween = null
